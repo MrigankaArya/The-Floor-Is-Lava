d2de235f270d960d4143de3a7c5c29c395283afc
Maggie Chen <maggieyudanchen@gmail.com>
2016-03-24 13:34:04 -0700

    Bind WASD, add axes

--- floor-is-lava.js
+++ floor-is-lava.js
@@ -1,3 +1,8 @@
+var basicMaterial = new THREE.MeshBasicMaterial({
+    color: 0xBBBBBB,
+    side: THREE.DoubleSide
+});
+
 // ASSIGNMENT-SPECIFIC API EXTENSION
 THREE.Object3D.prototype.setMatrix = function(a) {
     this.matrix = a;
@@ -36,28 +41,113 @@ window.onscroll = function() {
     window.scrollTo(0, 0);
 }
 
-// SETUP HELPER GRID
-// Note: Press Z to show/hide
-var gridGeometry = new THREE.Geometry();
-var i;
-for (i = -50; i < 51; i += 2) {
-    gridGeometry.vertices.push(new THREE.Vector3(i, 0, -50));
-    gridGeometry.vertices.push(new THREE.Vector3(i, 0, 50));
-    gridGeometry.vertices.push(new THREE.Vector3(-50, 0, i));
-    gridGeometry.vertices.push(new THREE.Vector3(50, 0, i));
+// Build a visual axis system
+function buildAxis(src, dst, colorHex, dashed) {
+    var geom = new THREE.Geometry(),
+        mat;
+
+    if (dashed) {
+        mat = new THREE.LineDashedMaterial({
+            linewidth: 3,
+            color: colorHex,
+            dashSize: 3,
+            gapSize: 3
+        });
+    } else {
+        mat = new THREE.LineBasicMaterial({
+            linewidth: 3,
+            color: colorHex
+        });
+    }
+
+    geom.vertices.push(src.clone());
+    geom.vertices.push(dst.clone());
+    geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+
+    var axis = new THREE.Line(geom, mat, THREE.LinePieces);
+
+    return axis;
+
 }
 
-var gridMaterial = new THREE.LineBasicMaterial({
-    color: 0xBBBBBB
-});
-var grid = new THREE.Line(gridGeometry, gridMaterial, THREE.LinePieces);
-scene.add(grid);
+function addAxes() {
+    var length = 100.0;
+    // Build axis visuliaztion for debugging.
+    x_axis = buildAxis(
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(length, 0, 0),
+        0xFF0000,
+        false
+    )
+    y_axis = buildAxis(
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, length, 0),
+        0x00ff00,
+        false
+    )
+    z_axis = buildAxis(
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, length),
+        0x0000FF,
+        false
+    )
+
+    // ADDING THE AXIS DEBUG VISUALIZATIONS
+    scene.add(x_axis);
+    scene.add(y_axis);
+    scene.add(z_axis);
+}
+
+function addGrid() {
+    // SETUP HELPER GRID
+    // Note: Press Z to show/hide
+    var gridGeometry = new THREE.Geometry();
+    var i;
+    for (i = -50; i < 51; i += 2) {
+        gridGeometry.vertices.push(new THREE.Vector3(i, 0, -50));
+        gridGeometry.vertices.push(new THREE.Vector3(i, 0, 50));
+        gridGeometry.vertices.push(new THREE.Vector3(-50, 0, i));
+        gridGeometry.vertices.push(new THREE.Vector3(50, 0, i));
+    }
+
+    var gridMaterial = new THREE.LineBasicMaterial({
+        color: 0xBBBBBB
+    });
+    var grid = new THREE.Line(gridGeometry, gridMaterial, THREE.LinePieces);
+    scene.add(grid);
+}
+
+function addGroundPlane() {
+    var planeGeometry = new THREE.PlaneGeometry(100, 100, 1);
+    var plane = new THREE.Mesh(planeGeometry, basicMaterial);
+    var rotateX90 = new THREE.Matrix4().makeRotationX(Math.PI / 2);
+    plane.setMatrix(rotateX90);
+    scene.add(plane);
+}
+
+addGrid();
+addAxes();
+//addGroundPlane();
+
+
 var keyboard = new THREEx.KeyboardState();
 var grid_state = true;
 var key;
-keyboard.domElement.addEventListener('keydown', function(event) {
-    //TODO: Bind keys
-});
+keyboard.domElement.addEventListener('keydown', onKeyDown);
+
+function onKeyDown(event) {
+    console.log(event.code);
+    // TO-DO: BIND KEYS TO YOUR CONTROLS      
+    if (keyboard.eventMatches(event, "w")) { // Reveal/Hide helper grid
+        console.log("w!");
+    } else if (keyboard.eventMatches(event, "a")) { // Reveal/Hide helper grid
+        console.log("a!");
+    } else if (keyboard.eventMatches(event, "s")) { // Reveal/Hide helper grid
+        console.log("s!");
+    } else if (keyboard.eventMatches(event, "d")) { // Reveal/Hide helper grid
+        console.log("d!");
+    }
+}
 
 function update() {
     requestAnimationFrame(update);
