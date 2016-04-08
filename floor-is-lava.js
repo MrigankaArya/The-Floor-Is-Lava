9ef52fd7fc3c70645145bd889fd89b15151b984b
Mriganka Arya <mrig.arya@gmail.com>
2016-04-07 19:03:08 -0700

    Picking wip

--- floor-is-lava.js
+++ floor-is-lava.js
@@ -12,6 +12,12 @@ var lavaSpeed = 0.0002;
 //INITIATE OBSTACLES
 var obstacles = [];
 
+//Any objects that the player can interact with go here eg: lava wheel, pillows etc.
+var interactables = [];
+
+var gameCanvas = $("canvas:first-child");
+var posNewX = gameCanvas.attr("width")/2;
+var posNewY = gameCanvas.attr("height")/2;
 
 // ASSIGNMENT-SPECIFIC API EXTENSION
 THREE.Object3D.prototype.setMatrix = function(a) {
@@ -249,11 +255,22 @@ function addRoom() {
     var frontTransform = new THREE.Matrix4().makeTranslation(0, levelHeight / 2, levelLength / 2);
     var frontWall = makeRoomSurface(levelWidth, levelHeight, 1, frontTransform);
 }
-
 addGrid();
 addAxes();
 addRoom();
 
+function makeWheel(){
+    console.log("ey wat");
+    var ringGeometry = new THREE.TorusGeometry(10, 0.2, 16, 100);
+    var ringMesh = new THREE.Mesh(ringGeometry, toonMaterial2);
+    var transformMatrix = new THREE.Matrix4().makeTranslation(levelWidth/2 - 10, levelHeight/2 - 10, levelLength/2-10);
+    ringMesh.setMatrix(transformMatrix);
+    obstacles.push(ringMesh);
+    interactables.push(ringMesh);
+    scene.add(ringMesh);
+}
+
+makeWheel();
 //Adds lava to the floor and deforms it as necessary.
 function addLava() {
     var lavaGeometry =  new THREE.PlaneGeometry(levelWidth, levelWidth, 129, 129);  //100 segments each 
@@ -494,7 +511,21 @@ function detectCollision(){
    }
 }
 
+//picking ray
+function pickRay(){
+    //use the canvas centre for the picking ray instead of the pointer
+    var center = new THREE.Vector2();
+    center.x = posNewX;
+    center.y = posNewY;
 
+    var pickRayCaster = new THREE.Raycaster();
+    pickRayCaster.setFromCamera(center, firstPersonCamera);
+
+    var intersects  = pickRayCaster.intersectObjects(obstacles);
+    for(var i = 0 ; i< intersects.length; i++){
+        //TODO
+    }
+}
 //Sort obstacles by y position for collision detection
 obstacles.sort(function(a, b) {
     return a.position.z - b.position.z;
@@ -684,6 +715,7 @@ function update() {
     //the +1 is to prevent the near plane of the camera from intersecting with the ground plane
 
     detectCollision();
+    pickRay();
     
     // Update health
     if (diff <= 0 && isInLava == false) {
@@ -751,5 +783,5 @@ function update() {
 }
 
 update();
-
+console.log(scene.children)
 }
